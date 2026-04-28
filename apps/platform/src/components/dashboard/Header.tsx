16f54bf0e82b3998b61435f9f3b73c00d537a1c8
afsuyadi <afsuyadi@gmail.com>
2026-04-28 20:13:09 +0700

    feat(component): style Header

--- apps/platform/src/components/dashboard/Header.tsx
+++ apps/platform/src/components/dashboard/Header.tsx
@@ -1,8 +1,8 @@
 export function Header() {
 	return (
-		<header className="flex flex-row">
-			<h1>Dashboard</h1>
-			<div>
+		<header className="flex flex-row justify-between p-3 bg-green-900 text-white">
+			<h1 className="font-bold">Dashboard</h1>
+			<div className="flex gap-7">
 				<span>🔔</span>
 				<span>👤 Admin</span>
 			</div>
