e61d7a339f0bacc33f74bc08842fb18e0683b4d9
afsuyadi <afsuyadi@gmail.com>
2026-04-30 07:34:34 +0700

    fix(hero): fix text size

--- apps/platform/src/components/landing/Hero.tsx
+++ apps/platform/src/components/landing/Hero.tsx
@@ -1,10 +1,15 @@
 export function Hero({ schoolName }: { schoolName: string }) {
 	return (
-		<section className="min-h-screen bg-white flex flex-col items-center justify-center text-center px-6">
-			<h1 className="text-sm text-green-700 mb-4">Welcome to {schoolName}</h1>
-			<p className="text-sm text-gray-400 mb-6">powered by Ihsanify</p>
+		<section className="gap-3 min-h-screen bg-white flex flex-col items-center justify-center text-center px-6">
+			<h1 className="text-4xl text-green-700 mb-4 font-bold">
+				Welcome to {schoolName}
+			</h1>
+			<p className="text-1xl text-gray-400 mb-6">Powered by Ihsanify.</p>
 			<p className="text-lg text-gray-600 mb-8">
-				<button type="button" className="bg-green-600 text-white px-8 py-3">
+				<button
+					type="button"
+					className="bg-green-600 text-white px-8 py-3 rounded-lg"
+				>
 					Get Started
 				</button>
 			</p>
