ae6be5ba122d76e6c5fce29ccce8e464db8837a6
afsuyadi <afsuyadi@gmail.com>
2026-08-04 23:03:36 +0700

    fix(routes): remove old routes file

--- apps/platform/src/routes/dashboard/assignments.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/dashboard/assignments")({
-	component: RouteComponent,
-});
-
-function RouteComponent() {
-	return <div>Hello "/dashboard/Assignments"!</div>;
-}
--- apps/platform/src/routes/dashboard/profile.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/dashboard/profile")({
-	component: RouteComponent,
-});
-
-function RouteComponent() {
-	return <div>Hello "/dashboard/Profile"!</div>;
-}
